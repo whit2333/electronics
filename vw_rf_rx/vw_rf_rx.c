9218c0000fdc30e9f818c93b998aabf891b4a1b0
Whitney Armstrong <whit@temple.edu>
2015-08-19 00:43:32 -0400

    modified:   SFgeiger/SFgeiger 	new file:   vw_rf_rx/GNUmakefile 	new file:   vw_rf_rx/VirtualWire.cpp 	new file:   vw_rf_rx/VirtualWire.h 	new file:   vw_rf_rx/vw_rf_rx.c 	new file:   vw_rf_tx/GNUmakefile 	new file:   vw_rf_tx/VirtualWire.cpp 	new file:   vw_rf_tx/VirtualWire.h 	new file:   vw_rf_tx/vw_rf_tx.c

---
+++ vw_rf_rx/vw_rf_rx.c
@@ -0,0 +1,62 @@
+#include "VirtualWire.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char buffer[80];
+
+const int led_pin = 13;
+const int transmit_pin = 12;
+const int receive_pin = 2;
+const int transmit_en_pin = 3;
+
+void setup()
+{
+    delay(1000);
+    Serial.begin(9600);	// Debugging only
+    Serial.println("setup");
+
+    sprintf(buffer, "VW_MAX_MESSAGE_LEN=%d", VW_MAX_MESSAGE_LEN);
+
+    Serial.println(buffer);
+
+    // Initialise the IO and ISR
+    vw_set_tx_pin(transmit_pin);
+    vw_set_rx_pin(receive_pin);
+    vw_set_ptt_pin(transmit_en_pin);
+    vw_set_ptt_inverted(true); // Required for DR3100
+    vw_setup(2000);	 // Bits per sec
+
+    vw_rx_start();       // Start the receiver PLL running
+
+    pinMode(led_pin, OUTPUT);
+}
+
+void loop()
+{
+    uint8_t buf[VW_MAX_MESSAGE_LEN];
+    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+
+    if (vw_get_message(buf, &buflen)) // Non-blocking
+    {
+	int i;
+
+        digitalWrite(led_pin, HIGH); // Flash a light to show received good message
+	// Message with a good checksum received, dump it.
+	Serial.print("Got: ");
+
+        snprintf(buffer, 6, "Got: %s", buf);
+        Serial.print(buffer);
+        sprintf(buffer,  "%d", (uint8_t)buf[6]);
+        Serial.print(buffer);
+
+        //for (i = 0; i < buflen; i++)
+        //{
+	//    Serial.print(((char *)buf)[i]);
+	//    Serial.print(' ');
+	//}
+
+	Serial.println();
+        digitalWrite(led_pin, LOW);
+    }
+}
+
